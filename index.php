6c9478b69211fddae474f3f017ac04a4373e5600
RuanMSantos <ruancauancauan@gmail.com>
2024-11-11 16:07:53 -0300

    Melhora design para meu amigo Breno

--- index.php
+++ index.php
@@ -4,27 +4,27 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>AULA PHP</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-
-    <form action="script.php" method="post">
-            
-    <label for="nome">Nome</label><br><br>
-    <input type="text" name="nome" id="nome">
-    <label for="nota1">Nota1</label><br><br>
+    <form action="script.php" method="post">        
+    
+    <label for="nome">Nome</label>
+    <input type="text" autocomplete="off" name="nome" id="nome">
+    
+    <label for="nota1">Nota 1:</label>
     <input type="number" name="nota1" id="nota1">
-    <label for="nota2">Nota2</label><br><br>
+    
+    <label for="nota2">Nota 2:</label>
     <input type="number" name="nota2" id="nota2">
-    <label for="nota3">Nota3</label><br><br>
+    
+    <label for="nota3">Nota 3:</label>
     <input type="number" name="nota3" id="nota3">
-    <label for="nota4">Nota4</label><br><br>
+    
+    <label for="nota4">Nota 4:</label>
     <input type="number" name="nota4" id="nota4">
+    
     <button type="submit">Enviar</button>
-
-
-
-
     </form>
-
 </body>
 </html>
